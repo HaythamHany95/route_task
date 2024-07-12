da1270fe776666c79d60e7ac15df3da97bbe9aec
HaythamHany95 <haythamhany95@gmail.com>
2024-07-12 19:58:37 +0300

    Functionality of Data & Domain layers Done ✅

--- lib/products_feature/domain/repository/repository/products_repository_contract.dart
+++ lib/products_feature/domain/repository/repository/products_repository_contract.dart
@@ -3,5 +3,5 @@ import 'package:route_task/products_feature/domain/entities/error.dart';
 import 'package:route_task/products_feature/domain/entities/products_response.dart';
 
 abstract class ProductsRepositoryContract {
-  Future<Either<ProductsResponse, Errors>> getProducts();
+  Future<Either<Errors, ProductsResponse>> getProducts();
 }
